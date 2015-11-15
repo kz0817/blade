233ef26e8c10afeb317a7de5bfdbbbe2f7be9e67
Kazuhiro Yamato <kz0817@gmail.com>
2015-11-15 17:09:59 +0900

    [ieserver-tool.py] Update only if needed.

--- bin/ieserver-tool.py
+++ bin/ieserver-tool.py
@@ -3,6 +3,7 @@
 import urllib
 import urllib2
 import argparse
+import socket
 
 IP_CHECK_URL = "http://ieserver.net/ipcheck.shtml"
 UPDATE_URL = "http://ieserver.net/cgi-bin/dip.cgi"
@@ -12,11 +13,22 @@ def main():
     parser.add_argument("username", type=str)
     parser.add_argument("password", type=str)
     parser.add_argument("--domain", type=str, default="dip.jp")
+    parser.add_argument("--page-code", type=str, default="euc_jp")
+    parser.add_argument("-f", "--force-update", action="store_true")
     args = parser.parse_args()
 
+    # Get the public IP address
     response = urllib2.urlopen(IP_CHECK_URL)
     my_addr = response.read()
     print "Public addr.: %s" % my_addr
+
+    # Get the public address
+    print "Force update: %s" % args.force_update
+    hostname = "%s.%s" % (args.username, args.domain)
+    dns_addr = socket.gethostbyname(hostname)
+    if not args.force_update and my_addr == dns_addr:
+        print "Skip IP addr. update"
+        return
 
     params = {
         "username": args.username,
@@ -26,7 +38,8 @@ def main():
     }
     url = UPDATE_URL + "?" + urllib.urlencode(params)
     response = urllib2.urlopen(url)
-    print response.read()
+    page_html = response.read()
+    print page_html.decode(args.page_code)
 
 if __name__ == "__main__":
     main()
