73b50f90601b070f45b9228a2d87d351a7b94d27
Kazuhiro Yamato <kz0817@gmail.com>
2021-03-07 16:15:54 +0900

    add a Markdown viewer 'kanamaru'

---
+++ bin/kanamaru.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+
+MORE_HELP='''
+Kanamaru, a markdown viewer
+
+This program uses markdown-it. You can install it as
+
+    npm install markdown-it --save
+'''
+
+HTML_TEMPLATE = '''
+<html>
+<head>
+<style type="text/css">
+<!--
+%s
+-->
+</style>
+</head>
+<body>
+%s
+</body>
+</html>
+'''
+
+CSS_COMMON = '''
+h1,h2 {
+  margin: 1.0rem 0.4rem;
+}
+h3,h4,h5,p {
+  margin: 0.6rem 0.4rem;
+}
+pre {
+  margin: 0.4rem;
+  padding: 0.6rem 0.8rem;
+  border: solid 1px;
+}
+code {
+  padding: 0.2em 0.4em;
+}
+pre code {
+  padding: 0em;
+}
+ul {
+  margin: 0em;
+}
+'''
+
+CSS_LIGHT = CSS_COMMON + '''
+h1,h2 {
+  border-bottom: solid 1px #e0e0e0;
+}
+pre {
+  border-color: #e0e0e0;
+  background-color: #f0f0f0;
+}
+code {
+  background-color: #f0f0f0;
+}
+'''
+
+CSS_DARK = CSS_COMMON + '''
+body {
+  color: #e0e0f0;
+  background-color: #202020;
+}
+h1,h2 {
+  border-bottom: solid 1px #505050;
+}
+pre {
+  border-color: #303030;
+  background-color: #080808;
+}
+code {
+  background-color: #080808;
+}
+'''
+
+CSS_MAP = {
+    'default': CSS_LIGHT,
+    'light': CSS_LIGHT,
+    'dark': CSS_DARK,
+}
+
+def create_html(args, body_core):
+    css = CSS_MAP[args.style]
+    with open(args.out_filename, 'w') as f:
+        html = HTML_TEMPLATE % (css, body_core)
+        f.write(html)
+
+def run(args):
+    cmd = ['npx', 'markdown-it', '/dev/stdin']
+
+    params = {
+        'input': args.infile.read().encode(),
+        'stdout': subprocess.PIPE,
+        'check': True,
+    }
+    result = subprocess.run(cmd, **params)
+    body_core = result.stdout.decode()
+
+    html = create_html(args, body_core)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type=argparse.FileType('r'), default='-')
+    parser.add_argument('--more-help', action='store_true')
+    parser.add_argument('-p', '--port', default=8000)
+    parser.add_argument('-o', '--out-filename')
+    parser.add_argument('-s', '--style', choices=CSS_MAP.keys(),
+                        default='default')
+    args = parser.parse_args()
+
+    if args.more_help:
+        print(MORE_HELP)
+        return
+
+    if args.out_filename is None:
+        args.out_filename = args.infile.name + '.html'
+
+    print(f'Input : {args.infile.name}')
+    print(f'Output: {args.out_filename}')
+    print(f'Style : {args.style}')
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
