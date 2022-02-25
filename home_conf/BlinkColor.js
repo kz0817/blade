1b9a072c28e34844b4068c3079b4eb68fcf39638
Kazuhiro Yamato <kz0817@gmail.com>
2022-02-25 14:03:27 +0900

    Add color theme for Blink.sh

---
+++ home_conf/BlinkColor.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#cc2020", "#499a06", "#edd400", "#3465a4", "#92659a", "#07c7ca", "#d3d7cf", "#808080", "#ff7070", "#8ae234", "#fce94f", "#729fcf", "#c19fbe", "#63e9e9", "#eeeeec"]);
+t.prefs_.set('foreground-color', "#eeeeec");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', '#eeeeec');
